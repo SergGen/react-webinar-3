ec245e06b70957fea903fa47c1fe7caed54c0e57
Sergey <se.gennp@gmail.com>
2023-05-23 15:08:11 +0300

    fix: Устранение замечаний
    Добавлен отступ в корзине.
    Хранение состояния количества товаров в корзине и их суммы вынесено в отдельное состояние.
    Убран скролл, когда не требуется.
    Модальное окно корзины вынесено компонент App из PgeLayout.

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import './style.css';
 import Head from "../head";
 import List from "../list";
-import {calcSum, formatNumber} from "../../utils";
+import {formatNumber} from "../../utils";
 export function Cart({cart, onAction, onClose}) {
   return (
     <>
       <div className={'background'}/>
-      <div className={'Cart'}>
-        <Head title='Корзина'>
-          <button className={'button_pointer'} onClick={onClose}>Закрыть</button>
-        </Head>
-        {cart.length > 0 ?
-          <div>
-            <List onAction={onAction} list={cart}/>
-            <div className={'total-block'}>
-              <span>Итого</span>
-              <span className={'total-value'}>{formatNumber(calcSum(cart))} &#8381;</span>
+      <div className={'cart-block'}>
+        <div className={'Cart'}>
+          <Head title='Корзина'>
+            <button className={'button_pointer'} onClick={onClose}>Закрыть</button>
+          </Head>
+          {cart.totalQuantity > 0 ?
+            <div className={'list-block'}>
+              <List onAction={onAction} list={cart.list}/>
+              <div className={'total-block'}>
+                <span>Итого</span>
+                <span className={'total-value'}>{formatNumber(cart.sum)} &#8381;</span>
+              </div>
             </div>
-          </div>
-           : <p className={'info-empty'}>Корзина пуста</p>}
+            : <p className={'info-empty'}>Корзина пуста</p>}
+        </div>
       </div>
     </>
   );
