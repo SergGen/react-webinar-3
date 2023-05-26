066dd765cbdf8aee55c411364ac80d7327d7ef16
Sergey <se.gennp@gmail.com>
2023-05-26 23:54:37 +0300

    feat: Add pagination.

--- src/components/page-list/index.js
+++ src/components/page-list/index.js
@@ -6,9 +6,10 @@ import useSelector from "../../store/use-selector";
 import Item from "../item";
 import useStore from "../../store/use-store";
 import {redirect, useParams} from "react-router-dom";
+import Pagination from "../pagination";
 
 function PageList() {
-  const { currentPage} = useParams();
+  const currentPage = useParams().currentPage || 1;
   const store = useStore();
   useEffect(() => {
     store.actions.catalog.load(currentPage).catch(() => { redirect('/page404'); });
@@ -38,7 +39,7 @@ function PageList() {
       <Head title='Магазин'/>
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
-      <p>PAGINATION {select.pageAmount}</p>
+      {select.pageAmount > 1 && <Pagination currentPage={currentPage} pageAmount={select.pageAmount} />}
     </>
   );
 }
