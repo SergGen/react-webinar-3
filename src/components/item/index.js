c0e3d339fa15aebb3185f3779b7e2af6ea49923e
Sergey <se.gennp@gmail.com>
2023-05-26 23:03:17 +0300

    feat: Add React router

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,4 +1,5 @@
-import {memo, useState} from "react";
+import {memo} from "react";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import {numberFormat} from "../../utils";
@@ -9,14 +10,13 @@ function Item(props){
   const cn = bem('Item');
 
   const callbacks = {
-    onAdd: (e) => props.onAdd(props.item._id)
+    onAdd: () => { props.onAdd(props.item._id); }
   }
 
   return (
     <div className={cn()}>
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
       <div className={cn('title')}>
-        {props.item.title}
+        <Link to={`/item/${props.item._id}`}>{props.item.title}</Link>
       </div>
       <div className={cn('actions')}>
         <div className={cn('price')}>{numberFormat(props.item.price)} ₽</div>
