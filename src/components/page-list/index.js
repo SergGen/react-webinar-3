dd00fd5fb976f3d58d4fc37965f628ba394851dd
Sergey <se.gennp@gmail.com>
2023-05-26 23:04:31 +0300

    feat: Add list page

---
+++ src/components/page-list/index.js
@@ -0,0 +1,46 @@
+import {memo, useCallback, useEffect} from "react";
+import Head from "../head";
+import BasketTool from "../basket-tool";
+import List from "../list";
+import useSelector from "../../store/use-selector";
+import Item from "../item";
+import useStore from "../../store/use-store";
+import {redirect, useParams} from "react-router-dom";
+
+function PageList() {
+  const { currentPage} = useParams();
+  const store = useStore();
+  useEffect(() => {
+    store.actions.catalog.load(currentPage).catch(() => { redirect('/page404'); });
+  }, []);
+
+  const select = useSelector(state => ({
+    list: state.catalog.list,
+    pageAmount: state.catalog.pageAmount,
+    amount: state.basket.amount,
+    sum: state.basket.sum
+  }));
+
+  const callbacks = {
+    // Добавление в корзину
+    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
+    // Открытие модалки корзины
+    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+  }
+
+  const renders = {
+    item: useCallback((item) => {
+      return <Item item={item} onAdd={callbacks.addToBasket}/>
+    }, [callbacks.addToBasket]),
+  };
+  return (
+    <>
+      <Head title='Магазин'/>
+      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}/>
+      <List list={select.list} renderItem={renders.item}/>
+      <p>PAGINATION {select.pageAmount}</p>
+    </>
+  );
+}
+
+export default memo(PageList)
