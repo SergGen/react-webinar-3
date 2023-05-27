42b03741fb52cb72c678f35c6920d911bf8bbdf2
Sergey <se.gennp@gmail.com>
2023-05-27 05:06:09 +0300

    feat: Добавлены стили странице 404

--- src/components/page-error/index.js
+++ src/components/page-error/index.js
@@ -1,8 +1,18 @@
 import {memo} from "react";
+import {Link} from "react-router-dom";
+import './style.css';
+import useSelector from "../../store/use-selector";
 
 function PageError() {
+  const select = useSelector(state => ({
+    notFound: state.translation.items.notFound[state.translation.current],
+  }));
+  console.log()
   return (
-    <h3>Страница не найдена!!!</h3>
+    <div className="PageError">
+      <h3>{select.notFound.message}</h3>
+      <Link to="/">{select.notFound.link}</Link>
+    </div>
   );
 }
 
