8ebbf2412ce9881d33ecc887fc4a038eae25085b
Sergey <se.gennp@gmail.com>
2023-05-29 10:48:31 +0300

    fix: Умные компоненты перемещены в app

--- src/app/page-list/index.js
+++ src/app/page-list/index.js
@@ -1,12 +1,10 @@
 import {memo, useCallback, useEffect} from "react";
-import Head from "../head";
-import BasketTool from "../basket-tool";
-import List from "../list";
+import List from "../../components/list";
 import useSelector from "../../store/use-selector";
-import Item from "../item";
+import Item from "../../components/item";
 import useStore from "../../store/use-store";
 import {redirect, useParams} from "react-router-dom";
-import Pagination from "../pagination";
+import Pagination from "../../components/pagination";
 
 function PageList() {
   const currentPage = Number(useParams().currentPage) || 1;
@@ -16,13 +14,8 @@ function PageList() {
     list: state.catalog.list,
     pageAmount: state.catalog.pageAmount,
     perPage: state.catalog.perPage,
-    amount: state.basket.amount,
-    sum: state.basket.sum,
-    head: state.translation.items.head[state.translation.current],
     item: state.translation.items.item[state.translation.current],
-    basketTool: state.translation.items.basketTool[state.translation.current],
     pagination: state.translation.items.pagination[state.translation.current],
-    current: state.translation.current
   }));
 
   useEffect(() => {
@@ -35,8 +28,6 @@ function PageList() {
     // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store.actions.basket]),
     // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store.actions.modals]),
-    changeTranslation: useCallback((lang) => store.actions.translation.setTranslation(lang), [store.actions.translation]),
     changePerPage: useCallback((newPerPage) => store.actions.catalog.setPerPage(newPerPage), [store])
   }
 
@@ -47,9 +38,6 @@ function PageList() {
   };
   return (
     <>
-      <Head translation={select.head} current={select.current} onChangeLang={callbacks.changeTranslation}/>
-      <BasketTool translation={select.basketTool} onOpen={callbacks.openModalBasket} amount={select.amount}
-                  sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
       {select.pageAmount > 1 &&
         <Pagination onChangePerPage={callbacks.changePerPage} translation={select.pagination} perPage={select.perPage}
