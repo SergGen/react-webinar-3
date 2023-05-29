2f28851ee12c070fc945adfdb21fa1b27320ac1a
Sergey <se.gennp@gmail.com>
2023-05-29 10:47:06 +0300

    fix: Переделан роутинг

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,11 +1,41 @@
-import {memo} from 'react';
+import {memo, useCallback} from 'react';
 import PageLayout from "../../components/page-layout";
-import {Outlet} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+import PageError from "../../components/page-error";
+import Head from "../../components/head";
+import BasketTool from "../../components/basket-tool";
+import PageList from "../page-list";
+import PageItem from "../page-item";
+import useStore from "../../store/use-store";
+import useSelector from "../../store/use-selector";
 
 function Main() {
+  const store = useStore();
+  const select = useSelector(state => ({
+    amount: state.basket.amount,
+    sum: state.basket.sum,
+    head: state.translation.items.head[state.translation.current],
+    item: state.translation.items.item[state.translation.current],
+    basketTool: state.translation.items.basketTool[state.translation.current],
+    pagination: state.translation.items.pagination[state.translation.current],
+    current: state.translation.current
+  }));
+  const callbacks = {
+    // Открытие модалки корзины
+    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store.actions.modals]),
+    changeTranslation: useCallback((lang) => store.actions.translation.setTranslation(lang), [store.actions.translation]),
+  }
   return (
     <PageLayout>
-      <Outlet/>
+      <Head translation={select.head} current={select.current} onChangeLang={callbacks.changeTranslation}/>
+      <BasketTool translation={select.basketTool} onOpen={callbacks.openModalBasket} amount={select.amount}
+                  sum={select.sum}/>
+      <Routes>
+        <Route path="/" element={<PageList />}/>
+        <Route path="/:currentPage" element={<PageList />}/>
+        <Route path="/item/:currentItemId" element={<PageItem />}/>
+        <Route path="/*" element={<PageError/>}/>
+      </Routes>
     </PageLayout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import {createRoot} from 'react-dom/client';
 import Store from "./store";
 import {StoreContext} from "./store/context";
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router";
-import Loading from "./components/Loading";
+import {BrowserRouter} from "react-router-dom";
+import App from "./app";
 
 const store = new Store();
 
@@ -12,6 +11,8 @@ const root = createRoot(document.getElementById('root'));
 // Первый рендер приложения
 root.render(
   <StoreContext.Provider value={store}>
-    <RouterProvider router={router} fallbackElement={<Loading />}/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>
 );
