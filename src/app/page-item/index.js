06bd4f902a8544014a1ab9954220816ea2e9969e
Sergey <se.gennp@gmail.com>
2023-05-29 10:48:31 +0300

    fix: Умные компоненты перемещены в app

--- src/app/page-item/index.js
+++ src/app/page-item/index.js
@@ -1,10 +1,8 @@
 import {memo, useCallback, useEffect} from "react";
 import {useParams, redirect} from "react-router-dom";
-import Head from "../head";
-import BasketTool from "../basket-tool";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
-import ItemDetail from "../item-detail";
+import ItemDetail from "../../components/item-detail";
 
 function PageItem() {
   const { currentItemId} = useParams();
@@ -13,8 +11,6 @@ function PageItem() {
     store.actions.item.load(currentItemId).catch(() => { redirect('/page404'); });
   }, [currentItemId]);
   const select = useSelector(state => ({
-    amount: state.basket.amount,
-    sum: state.basket.sum,
     item: state.item.info,
     basketTool: state.translation.items.basketTool[state.translation.current],
     itemTranslation: state.translation.items.item[state.translation.current],
@@ -23,17 +19,10 @@ function PageItem() {
   const callbacks = {
     // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store.actions.basket]),
-    // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store.actions.modals]),
-    changeTranslation: useCallback((lang) => store.actions.translation.setTranslation(lang), [store.actions.translation])
   }
 
   return (
-    <>
-      <Head translation={select.item} current={select.current} onChangeLang={callbacks.changeTranslation}/>
-      <BasketTool translation={select.basketTool} onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum}/>
       <ItemDetail translation={select.itemTranslation} item={select.item} onAdd={callbacks.addToBasket}/>
-    </>
   );
 }
 
